694247ba13887a8548c6bfcc2cf73f5d9a5640bc
milklot <vbiostflex@gmail.com>
2020-10-17 14:08:01 -0400

    deleted mine console.log(s)

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -6,8 +6,6 @@ const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
-  console.log({comments});
-
   return (
     <div>
       {

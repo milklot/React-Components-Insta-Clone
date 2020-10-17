204ce867a923af522b678e4bdcf450075148171a
milklot <vbiostflex@gmail.com>
2020-10-17 12:46:55 -0400

    done with app and posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-/* 
-  Start here and work your way down the nested components.
-  Not all files in the project need code added.
-  Look at each file to see what props need to be passed!
-*/
 
 // Import the state hook
-import React from 'react';
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
-// Import the dummyData
+import React, { useState } from 'react';
 import './App.css';
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
+import dummyData from './dummy-data'
 
 const App = () => {
+  const [ posts, setPosts] = useState(dummyData);
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
+    setPosts(posts.map((obj) => {
+      if (obj.id === postId){
+        return {...obj, likes: obj.likes++}
+      }
+      else {
+        return obj;
+      }
+  }))
+};
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -27,10 +33,12 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+
 
   return (
     <div className='App'>
+      <SearchBar />
+      <Posts likePost = {likePost} posts = {posts}/>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
